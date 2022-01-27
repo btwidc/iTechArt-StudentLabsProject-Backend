ef297588e3d58fdd7e2e4fc857f9a3759d7509e9
btwidc <krauchankakiryl@tut.by>
2022-01-27 16:05:38 +0300

    feat: connect database using sequelize and add data model to user

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
+require("dotenv").config();
 import express from "express";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
+const sequelize = require("./db");
+const models = require("./models/models");
 
-app.get("/", (req, res) => {
-  res.send("Started!");
-});
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(port, () => {
+      console.log(`App listening on port ${port}`);
+    });
+    app.get("/", (req, res) => {
+      res.send("Started!");
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+start();
