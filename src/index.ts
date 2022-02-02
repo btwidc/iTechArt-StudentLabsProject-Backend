b57f0a40ba904bcf538f8eef6740c57e04fab06d
btwidc <krauchankakiryl@tut.by>
2022-02-02 13:16:17 +0300

    fix: add domain for sharing cookies

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const sequelize = require("./db");
 const port = process.env.PORT || 4000;
 
 const app = express();
-app.use(cors());
+app.use(cors({ credentials: true, origin: process.env.WEB_CLIENT_URL }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", router);
