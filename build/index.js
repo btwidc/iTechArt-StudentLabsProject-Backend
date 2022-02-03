9a710ffef853d1c613cdece5be7ce123d958e5b7
btwidc <krauchankakiryl@tut.by>
2022-02-03 13:34:54 +0300

    refactor: set local domain

---
+++ build/index.js
@@ -0,0 +1,42 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv").config();
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const index_1 = __importDefault(require("./routes/index"));
+const errorHandlingMiddleware = require("./middleware/errorHandlingMiddleware");
+const sequelize = require("./db");
+const port = process.env.PORT || 4000;
+const app = express_1.default();
+app.use(cors_1.default({ credentials: true, origin: process.env.WEB_CLIENT_URL }));
+app.use(express_1.default.json());
+app.use(cookie_parser_1.default());
+app.use("/api", index_1.default);
+app.use(errorHandlingMiddleware);
+const start = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield sequelize.authenticate();
+        yield sequelize.sync();
+        app.listen(port, () => {
+            console.log(`App listening on port ${port}`);
+        });
+    }
+    catch (e) {
+        console.log(e);
+    }
+});
+start();
+//# sourceMappingURL=index.js.map
---
+++ build/services/tokenService.js
@@ -0,0 +1,65 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const { Token } = require("../models/models");
+class TokenService {
+    generateTokens(payload) {
+        const accessToken = jsonwebtoken_1.default.sign(payload, process.env.JWT_ACCESS_SECRET, {
+            expiresIn: "30m",
+        });
+        const refreshToken = jsonwebtoken_1.default.sign(payload, process.env.JWT_REFRESH_SECRET, {
+            expiresIn: "30d",
+        });
+        return { accessToken, refreshToken };
+    }
+    validateAccessToken(token) {
+        try {
+            return jsonwebtoken_1.default.verify(token, process.env.JWT_ACCESS_SECRET);
+        }
+        catch (e) {
+            return null;
+        }
+    }
+    validateRefreshToken(token) {
+        try {
+            return jsonwebtoken_1.default.verify(token, process.env.JWT_REFRESH_SECRET);
+        }
+        catch (e) {
+            return null;
+        }
+    }
+    saveToken(userId, refreshToken) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const tokenData = yield Token.findOne({ where: { userId } });
+            if (tokenData) {
+                tokenData.refreshToken = refreshToken;
+                return tokenData.save();
+            }
+            return yield Token.create({ userId, refreshToken });
+        });
+    }
+    removeToken(refreshToken) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield Token.destroy({ where: { refreshToken } });
+        });
+    }
+    findToken(refreshToken) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield Token.findOne({ where: { refreshToken } });
+        });
+    }
+}
+module.exports = new TokenService();
+//# sourceMappingURL=tokenService.js.map
