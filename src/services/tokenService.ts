527179ed02d0090274f1b76a289fc3b84935e52d
btwidc <krauchankakiryl@tut.by>
2022-02-06 23:11:30 +0300

    refactor: change access token expire time

--- src/services/tokenService.ts
+++ src/services/tokenService.ts
@@ -5,7 +5,7 @@ const { Token } = require("../models/models");
 class TokenService {
   generateTokens(payload) {
     const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
-      expiresIn: "30s",
+      expiresIn: "30000",
     });
     const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
       expiresIn: "30d",
