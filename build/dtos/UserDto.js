1f8a3a10a0286a1285e9a54e6b0b2f022f47f731
btwidc <krauchankakiryl@tut.by>
2022-02-09 21:47:25 +0300

    add build to gitignore

--- build/dtos/UserDto.js
+++
@@ -1,8 +0,0 @@
-module.exports = class UserDto {
-    constructor(model) {
-        this.email = model.email;
-        this.id = model.id;
-        this.role = model.role;
-    }
-};
-//# sourceMappingURL=UserDto.js.map
