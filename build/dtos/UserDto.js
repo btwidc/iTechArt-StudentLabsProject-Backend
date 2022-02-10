f0e4b73bd1ca0131a117f1d429bfab1cfd35bf4b
btwidc <krauchankakiryl@tut.by>
2022-02-10 21:16:09 +0300

    delete node and build

---
+++ build/dtos/UserDto.js
@@ -0,0 +1,8 @@
+module.exports = class UserDto {
+    constructor(model) {
+        this.email = model.email;
+        this.id = model.id;
+        this.role = model.role;
+    }
+};
+//# sourceMappingURL=UserDto.js.map
---
+++ build/routes/usersRouter.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const userController_1 = __importDefault(require("../controllers/userController"));
+const { body } = require("express-validator");
+const authorizationMiddleware = require("../middleware/authorizationMiddleware");
+const usersRouter = express_1.Router();
+usersRouter.post("/registration", body("email").isEmail(), body("password").isLength({ min: 6, max: 32 }), userController_1.default.registration);
+usersRouter.post("/login", userController_1.default.login);
+usersRouter.post("/logout", userController_1.default.logout);
+usersRouter.post("/refresh", userController_1.default.refresh);
+usersRouter.get("/test", authorizationMiddleware, userController_1.default.test);
+exports.default = usersRouter;
+//# sourceMappingURL=usersRouter.js.map
