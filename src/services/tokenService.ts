33d69635bcf7d3b329d7ed950d5398be78250a9d
btwidc <krauchankakiryl@tut.by>
2022-03-20 18:30:46 +0300

    refactor: change tokens expire time

--- src/services/tokenService.ts
+++ src/services/tokenService.ts
@@ -8,10 +8,10 @@ import ValidateResponse from '../types/ValidateResponse';
 class TokenService {
   public generateTokens(payload): TokensResponse {
     const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
-      expiresIn: '1m',
+      expiresIn: '30m',
     });
     const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
-      expiresIn: '5m',
+      expiresIn: '30d',
     });
     const tokens = { accessToken, refreshToken };
     return { ...tokens };
