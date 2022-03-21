36d2e84467803de792ff640f501af701201b4da1
btwidc <krauchankakiryl@tut.by>
2022-03-21 13:28:41 +0300

    last little fix

--- src/dtos/CandidateDto.ts
+++ src/dtos/CandidateDto.ts
@@ -1,27 +1,27 @@
 import Candidate from '../models/Candidate';
 
 class CandidateDto extends Candidate {
-    public id: number;
-    public name: string;
-    public surname: string;
-    public email: string;
-    public skype: string;
-    public phone: string;
-    public education: string;
-    public technology: string;
-    public cvName?: string;
-    constructor(model) {
-        super();
-        this.id = model.id;
-        this.name = model.name;
-        this.surname = model.surname;
-        this.email = model.email;
-        this.skype = model.skype;
-        this.phone = model.phone;
-        this.education = model.education;
-        this.technology = model.technology;
-        this.cvName = model?.cvName;
-    }
+  public id: number;
+  public name: string;
+  public surname: string;
+  public email: string;
+  public skype: string;
+  public phone: string;
+  public education: string;
+  public technology: string;
+  public cvName?: string;
+  constructor(model) {
+    super();
+    this.id = model.id;
+    this.name = model.name;
+    this.surname = model.surname;
+    this.email = model.email;
+    this.skype = model.skype;
+    this.phone = model.phone;
+    this.education = model.education;
+    this.technology = model.technology;
+    this.cvName = model?.cvName;
+  }
 }
 
 export default CandidateDto;
--- src/services/candidatesService.ts
+++ src/services/candidatesService.ts
@@ -9,105 +9,103 @@ import ApiError from '../errors/ApiError';
 export const docsPath = 'public/docs/';
 
 class CandidatesService {
-    public async addCandidate(
-        name: string,
-        surname: string,
-        email: string,
-        skype: string,
-        phone: string,
-        education: string,
-        technology: string,
-        cv?: File,
-    ): Promise<CandidateDto> {
-        const candidate = await this.addCandidateInfo(
-            name,
-            surname,
-            email,
-            skype,
-            phone,
-            education,
-            technology,
-        );
+  public async addCandidate(
+    name: string,
+    surname: string,
+    email: string,
+    skype: string,
+    phone: string,
+    education: string,
+    technology: string,
+    cv?: File,
+  ): Promise<CandidateDto> {
+    const candidate = await this.addCandidateInfo(
+      name,
+      surname,
+      email,
+      skype,
+      phone,
+      education,
+      technology,
+    );
 
-        if (cv) {
-            const candidateFullName = `${candidate.id}_${name}_${surname}`;
-            const cvName = `${candidateFullName}_${cv.name}`;
-            const cvPath = `${docsPath}${cvName}`;
-            cv.mv(cvPath);
+    if (cv) {
+      const candidateFullName = `${candidate.id}_${name}_${surname}`;
+      const cvName = `${candidateFullName}_${cv.name}`;
+      const cvPath = `${docsPath}${cvName}`;
+      cv.mv(cvPath);
 
-            candidate.cvName = cvName;
-            await candidate.save();
+      candidate.cvName = cvName;
+      await candidate.save();
 
-            return new CandidateDto(candidate);
-        } else {
-            return new CandidateDto(candidate);
-        }
+      return new CandidateDto(candidate);
+    } else {
+      return new CandidateDto(candidate);
+    }
+  }
+
+  public async addCandidateInfo(
+    name: string,
+    surname: string,
+    email: string,
+    skype: string,
+    phone: string,
+    education: string,
+    technology: string,
+  ): Promise<CandidateInfo> {
+    const candidate = await Candidate.findOne({ where: { email } });
+
+    if (candidate) {
+      throw ApiError.BadRequest(`Candidate with email ${email} already exists`);
     }
 
-    public async addCandidateInfo(
-        name: string,
-        surname: string,
-        email: string,
-        skype: string,
-        phone: string,
-        education: string,
-        technology: string,
-    ): Promise<CandidateDto> {
-        const candidate = await Candidate.findOne({ where: { email } });
+    const candidateData = await Candidate.create({
+      name,
+      surname,
+      email,
+      skype,
+      phone,
+      education,
+      technology,
+    });
+    return candidateData;
+  }
 
-        if (candidate) {
-            throw ApiError.BadRequest(
-                `Candidate with email ${email} already exists`,
-            );
-        }
+  public async getCandidatesList(): Promise<Array<CandidateInfo>> {
+    const candidates = await Candidate.findAll();
 
-        const candidateData = await Candidate.create({
-            name,
-            surname,
-            email,
-            skype,
-            phone,
-            education,
-            technology,
-        });
-        return new CandidateDto(candidateData);
+    if (!candidates) {
+      throw ApiError.BadRequest(`Can't get candidates list`);
     }
 
-    public async getCandidatesList(): Promise<Array<CandidateInfo>> {
-        const candidates = await Candidate.findAll();
+    return candidates;
+  }
 
-        if (!candidates) {
-            throw ApiError.BadRequest(`Can't get candidates list`);
-        }
+  public async getCandidateInfo(id: number): Promise<CandidateDto> {
+    const candidate = await Candidate.findOne({ where: { id } });
 
-        return candidates;
+    if (!candidate) {
+      throw ApiError.BadRequest(`Can't get candidate info`);
     }
 
-    public async getCandidateInfo(id: number): Promise<CandidateDto> {
-        const candidate = await Candidate.findOne({ where: { id } });
+    return new CandidateDto(candidate);
+  }
 
-        if (!candidate) {
-            throw ApiError.BadRequest(`Can't get candidate info`);
-        }
+  public async deleteCandidate(id: number): Promise<number> {
+    const candidate = await this.getCandidateInfo(id);
 
-        return new CandidateDto(candidate);
+    const deleteCandidateInfo = await Candidate.destroy({ where: { id } });
+    if (!deleteCandidateInfo) {
+      throw ApiError.BadRequest('Candidate does not exist');
     }
 
-    public async deleteCandidate(id: number): Promise<number> {
-        const candidate = await this.getCandidateInfo(id);
-
-        const deleteCandidateInfo = await Candidate.destroy({ where: { id } });
-        if (!deleteCandidateInfo) {
-            throw ApiError.BadRequest('Candidate does not exist');
-        }
-
-        const cvName = candidate.cvName;
-        if (cvName) {
-            const cvPath = `${docsPath}${cvName}`;
-            fs.unlinkSync(cvPath);
-        }
-        return deleteCandidateInfo;
+    const cvName = candidate.cvName;
+    if (cvName) {
+      const cvPath = `${docsPath}${cvName}`;
+      fs.unlinkSync(cvPath);
     }
+    return deleteCandidateInfo;
+  }
 }
 
 export default new CandidatesService();
