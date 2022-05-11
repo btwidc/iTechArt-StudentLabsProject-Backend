175bb7391aade692fa6ed14f71ff307af413630a
btwidc <krauchankakiryl@tut.by>
2022-05-11 10:17:53 +0300

    feat: add websocket server for live chat

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
+import http from 'http';
 import express from 'express';
 import cors from 'cors';
+
+import { Server } from 'socket.io';
+
 import fileUpload from 'express-fileupload';
 const cloudinary = require('cloudinary').v2;
 
 import router from './routes/index';
 import sequelize from './db';
+
 import errorHandlingMiddleware from './middleware/errorHandlingMiddleware';
 import synchronizeModels from './models/synchronizeModels';
 
@@ -21,17 +27,42 @@ cloudinary.config({
 });
 
 app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin:
+      'https://i-tech-art-student-labs-project-frontend-1palfcusu.vercel.app/',
+    methods: ['GET', 'POST'],
+  },
+});
+
 app.use(express.json());
 app.use(fileUpload({}));
 
 app.use('/api', router);
 app.use(errorHandlingMiddleware);
 
+const websocketServer = async () => {
+  io.on('connection', (socket) => {
+    socket.on('join_chat', (data) => {
+      socket.join(data);
+    });
+
+    socket.on('send_message', (data) => {
+      socket.to(data.room).emit('receive_message', data);
+    });
+  });
+};
+
 const start = async () => {
   try {
     await sequelize.authenticate();
     await synchronizeModels();
-    await app.listen(port, () => {
+
+    await websocketServer();
+    await server.listen(port, () => {
       console.error(`App listening on port ${port}`);
     });
   } catch (e) {
